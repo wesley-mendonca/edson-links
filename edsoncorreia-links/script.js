4f6862425efcdaebf7d72af50e399c7f2e8c27c7
Wesley Mendonça <125772028+wesley-mendonca@users.noreply.github.com>
2023-02-18 22:32:06 -0300

    Add files via upload

---
+++ edsoncorreia-links/script.js
@@ -0,0 +1,18 @@
+//function toggleMode() {
+  //const html = document.documentElement
+ // html.classList.toggle("light")
+
+  // pegar a tag img
+ // const img = document.querySelector("#profile img")
+
+  // substituir a imagem
+  //if (html.classList.contains("light")) {
+    // se tiver light mode, adicionar a imagem light
+  //  img.setAttribute("src", "./Avatar.png")
+//  } else {
+    // set tiver sem light mode, manter a imagem normal
+//    img.setAttribute("src", "./assets/Edson.png")
+ // }
+//}
+
+/////Neste projeto temos a opção de ser usado no d dark-mode e também no lightmode. Mas a pedido do cliente ele só quis no dark-mode mesmo! - Wesley Mendonça
